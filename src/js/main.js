848e2046bb2514091cac5bf34ccef7ef1f639cb9
Jacek Ruszkowski <jac.ruszkowski@gmail.com>
2022-03-22 09:15:35 +0100

    Ready without js

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,19 @@
 const burgerBtn = document.querySelector('.nav__mobile-burger')
 const closeBtn = document.querySelector('.nav__mobile-close')
 const navMobile = document.querySelector('.nav__mobile')
+const swup = new Swup()
+
+console.log(swup);
 
 const showNavigation = () => {
     navMobile.classList.add('show-nav')
-    if (navMobile.classList.contains('show-nav')) {
-        burgerBtn.style.display = 'none'
-        closeBtn.style.display = 'block'
-    } 
+    closeBtn.style.display = 'block'
 }
 
 const closeNavigation = () => {
     navMobile.classList.remove('show-nav')
-    burgerBtn.style.display = 'block'
     closeBtn.style.display = 'none'
 }
-
 
 burgerBtn.addEventListener('click', showNavigation)
 closeBtn.addEventListener('click', closeNavigation)
