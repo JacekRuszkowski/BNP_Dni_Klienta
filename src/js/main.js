6e04ecb4db00d40ffc8bc0270af4e902a244f5cd
Jacek Ruszkowski <jac.ruszkowski@gmail.com>
2022-04-19 13:00:26 +0200

    Final commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,16 @@
+///// VARIABLES //////
 const burgerBtn = document.querySelector('.nav__mobile-burger')
 const closeBtn = document.querySelector('.nav__mobile-close')
 const navMobile = document.querySelector('.nav__mobile')
-const swup = new Swup()
+const popupRegister = document.querySelector('.popup')
+const btnRegister = document.querySelectorAll('.day__register-btn')
+const windowRegister = document.querySelector('.popup__window')
+const registerCloseBtn = document.querySelector('.popup__close-btn')
+const registerSendBtn = document.querySelector('.popup__send-btn')
 
-console.log(swup);
 
+
+//// FUNCTIONS //////
 const showNavigation = () => {
     navMobile.classList.add('show-nav')
     closeBtn.style.display = 'block'
@@ -15,5 +21,27 @@ const closeNavigation = () => {
     closeBtn.style.display = 'none'
 }
 
+const showPopup = () => {
+    popupRegister.classList.add('opacity')
+    windowRegister.classList.add('transform')
+}
+
+const closePopup = () => {
+    popupRegister.classList.remove('opacity')
+    windowRegister.classList.remove('transform')
+}
+
+
+
+//// EVENTS /////
+
 burgerBtn.addEventListener('click', showNavigation)
 closeBtn.addEventListener('click', closeNavigation)
+
+btnRegister.forEach(button => {
+    button.addEventListener('click', showPopup)
+})
+
+registerCloseBtn.addEventListener('click', closePopup)
+registerSendBtn.addEventListener('click', closePopup)
+
